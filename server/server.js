8c51edad0739532fecc4f70d16a638877097eca7
Rounak Asati <johncenameranaam@gmail.com>
2026-04-27 14:52:50 +0530

    git import corrected

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-const passport = require("./config/passport");
+const passport = require("./utils/passport");
 const session = require("express-session");
 
 const app = express();   // ✅ FIRST create app
